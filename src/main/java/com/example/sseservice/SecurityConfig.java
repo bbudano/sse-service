6aa3375bedf419ca922fd7e9620daef577c417a0
Bernard Budano <bbudano@gmail.com>
2022-12-29 15:31:11 +0100

    Added spring security to user actual user as key for emitters map

---
+++ src/main/java/com/example/sseservice/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.sseservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user")
+                        .password("{noop}user")
+                        .roles("USER")
+                        .build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+}
--- src/main/java/com/example/sseservice/SseController.java
+++ src/main/java/com/example/sseservice/SseController.java
@@ -1,9 +1,9 @@
 package com.example.sseservice;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -15,8 +15,8 @@ public class SseController {
     private final SseHandler sseHandler;
 
     @GetMapping
-    public SseEmitter register(@RequestParam String user) {
-        return sseHandler.register(user);
+    public SseEmitter register(Authentication authentication) {
+        return sseHandler.register(authentication.getName());
     }
 
 }
